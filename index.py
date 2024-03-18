b584f8ca7186829884934cd59ef245911503a781
OngJunLe <ongjunle3@gmail.com>
2024-03-18 21:07:29 +0800

    Added total documents, changed all term frequencies to log values

--- index.py
+++ index.py
@@ -17,6 +17,7 @@ from collections import defaultdict
 # Format of postings [(term, skipID), ...]
 
 DOCUMENT_LENGTH_KEY = -100
+TOTAL_DOCUMENTS_KEY = -200
 
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d temp_postings-file -p postings-file")
@@ -34,10 +35,11 @@ def build_index(in_dir, out_dict, out_postings):
     term_dictionary = {}
     doc_length_dictionary = {}
     stemmer = nltk.stem.PorterStemmer()
-
+    total_documents = 0
 
     for fileid in os.listdir(in_dir): 
         file_ids.append(f"training/{fileid}")
+        total_documents += 1
     # Creates out_postings file so that it can be read later
     file = open(out_postings, "wb")
     file.close()
@@ -47,9 +49,9 @@ def build_index(in_dir, out_dict, out_postings):
         words = [stemmer.stem(word).lower() for word in words if word not in string.punctuation] 
         word_count = Counter(words)
         id = int(fileid.split("/")[-1])
-        # Create tuples of (docID, term freqeuncy) and appending to temp_postings
+        # Create tuples of (docID, log term freqeuncy) and appending to temp_postings
         for word in word_count:
-            temp_postings[word].append((id, word_count[word]))
+            temp_postings[word].append((id, 1 + math.log10(word_count[word])))
         # Document length calculation 
         sum = 0
         for word in word_count:
@@ -64,17 +66,19 @@ def build_index(in_dir, out_dict, out_postings):
         # Add document length dictionary
         dictionary_binary = pickle.dumps(doc_length_dictionary)
         no_of_bytes = len(dictionary_binary)
-        term_dictionary[DOCUMENT_LENGTH_KEY] = (current_offset, no_of_bytes, len(doc_length_dictionary))
+        term_dictionary[DOCUMENT_LENGTH_KEY] = (current_offset, no_of_bytes)
         output.write(dictionary_binary)
         current_offset += no_of_bytes
         for key in sorted_keys:
             to_add = sorted(temp_postings[key])
             to_add_binary = pickle.dumps(to_add)
             no_of_bytes = len(to_add_binary)
-            term_dictionary[key] = (current_offset, no_of_bytes, len(to_add))
+            term_dictionary[key] = (current_offset, no_of_bytes)
             output.write(to_add_binary)
             current_offset += no_of_bytes
 
+    # Add total number of documents to dictionary
+    term_dictionary[TOTAL_DOCUMENTS_KEY] = total_documents
     dictionary_binary = pickle.dumps(term_dictionary)
     with open(out_dict, "wb") as output:
         output.write(dictionary_binary)
@@ -107,15 +111,17 @@ if (__name__ == "__main__"):
         sys.exit(2)
 
     build_index(input_directory, output_file_dictionary, output_file_postings)
-    print ("testing")
+    
+'''
     with open("dictionary.txt","rb") as input:
         dictionary = pickle.loads(input.read())
-    current_offset, no_of_bytes, length = dictionary["bahia"]
+    current_offset, no_of_bytes = dictionary["bahia"]
     with open("postings.txt", "rb") as input:
         input.seek(current_offset)
         posting = pickle.loads(input.read(no_of_bytes))
         print(posting)
-
+    print(f"Total documents: {dictionary[TOTAL_DOCUMENTS_KEY]}")
+'''
   
 
 
